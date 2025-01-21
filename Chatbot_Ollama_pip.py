f53d52ed7e84d0559d7fcea47fd455864f2f7558
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2025-01-21 14:06:23 -0300

    fluid chatbot
    in this version the model receives previous interactions as complement to the imput, so the conversation can flow easily

--- Chatbot_Ollama_pip.py
+++ Chatbot_Ollama_pip.py
@@ -5,24 +5,36 @@ requirements:
       import ollama
       ollama.pull('ModelName')
 """
-
 from ollama import chat
+
+global ConvHist
+ConvHist=[]
+
 def talk(In):
-  
+  prompt ="ConversationHostory: \n"+str(ConvHist)+ In
   stream = chat(
      model='llama3.2',
-     messages=[{'role': 'user', 'content': In}],
-      stream=True,
+     
+     messages=[{'role': 'user', 'content': prompt}],
+      stream=True
 )
+  AiAnswer="AI: "
   print("AI: ",end="")
   for chunk in stream:
     print(chunk['message']['content'], end='', flush=True)
+    AiAnswer +=chunk['message']['content']+' '
+  ConvHist.append(AiAnswer)
+  
   
 
 while True:
-  print("\ntype end to exit chatbot.")
+  print("\ntype 'Hist' to see past interactions or 'end' to exit chatbot.")
   UserIn = input("User: ")
+  ConvHist.append("User: "+UserIn)
   if UserIn=='end':
     break
+  elif UserIn=="Hist":
+    print(ConvHist)
+    continue
 
   talk(UserIn)
